089c5e48bc8ab5a7a6fe2fba0e9eb493973afc72
mitch codes <mitchkodes@gmail.com>
2022-08-07 05:37:10 -0700

    python console

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,18 @@ import uuid
 from datetime import datetime
 
 class BaseModel():
+    """this class creates a base"""
 
     def __init__(self):
+        """initial class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow().isoformat()
         self.updated_at = datetime.utcnow().isoformat()
-        self.__dict__['__class__'] = 'BaseModel'
+        self.__dict__['__class__'] = __class__.__name__
+        print(self.__dict__)
 
     def save(self):
+        """update the time updated"""
         self.updated_at = datetime.today().isoformat()
+
+m = BaseModel()
