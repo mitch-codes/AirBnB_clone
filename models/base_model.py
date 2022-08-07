3e35425af86d047f264bf3a2ae8b24106eeb9b6c
mitch codes <mitchkodes@gmail.com>
2022-08-07 05:23:15 -0700

    python console

--- models/base_model.py
+++ models/base_model.py
@@ -6,8 +6,8 @@ class BaseModel():
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.utcnow().isoformat()
+        self.updated_at = datetime.utcnow().isoformat()
         self.__dict__['__class__'] = 'BaseModel'
 
     def save(self):
