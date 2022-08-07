e336e01a355d58df582d5ef2a73796c7b5c03f32
mitch codes <mitchkodes@gmail.com>
2022-08-07 07:09:35 -0700

    python console

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,15 @@ from datetime import datetime
 class BaseModel():
     """this class creates a base"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initial class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow().isoformat()
-        self.updated_at = self.created_at
+        if kwargs is not None:
+            for key, value in Kwargs.items():
+                self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow().isoformat()
+            self.updated_at = self.created_at
 
     def save(self):
         """update the time updated"""
