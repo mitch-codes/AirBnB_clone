5ad392cdd928f5a076d9452bf65f455dd36f6e96
mitch codes <mitchkodes@gmail.com>
2022-08-07 09:14:36 -0700

    python console

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3iiiiiiii
 """base of the project"""
 import uuid
 from datetime import datetime
@@ -9,8 +9,8 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """initial class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow().isoformat()
+        self.id = str(uuid.uuid4()) 
+        self.created_at = datetime.now()
         self.updated_at = self.created_at
         if kwargs is not None:
             for key, value in kwargs.items():
@@ -18,7 +18,7 @@ class BaseModel():
 
     def save(self):
         """update the time updated"""
-        self.updated_at = datetime.utcnow().isoformat()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """change the __str__ variable"""
@@ -29,6 +29,9 @@ class BaseModel():
         """create personal dict from __dict__"""
         my_dict = {}
         for key, value in self.__dict__.items():
-            my_dict[key] = value
-        my_dict['__class__'] = self.__class__.__name__
+            if key == 'created_at' or key == 'updated_at':
+                my_dict[key] = value.isoformat()
+            else:
+                my_dict[key] = value
+            my_dict['__class__'] = self.__class__.__name__
         return my_dict
