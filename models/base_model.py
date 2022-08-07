c171ae6d21f258001293d790d046fc2805a5fc5f
mitch codes <mitchkodes@gmail.com>
2022-08-07 06:19:28 -0700

    python console

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,14 @@ class BaseModel():
         """initial class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow().isoformat()
-        self.updated_at = datetime.utcnow().isoformat()
+        self.updated_at = self.created_at
         self.__dict__['__class__'] = __class__.__name__
+        print(self.__str__)
 
     def save(self):
         """update the time updated"""
-        self.updated_at = datetime.today().isoformat()
+        self.updated_at = datetime.utcnow().isoformat()
+
+    def __str__(self):
+        """change the __str__ variable"""
+        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
