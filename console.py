79bc427f71a0632f1d53ad82081752cb6103a8f7
mitch codes <mitchkodes@gmail.com>
2022-08-07 16:17:52 -0700

    python summary

--- console.py
+++ console.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import cmd
-import sys
 
 """command line tool in python"""
 
 
 class HBNBCommand(cmd.Cmd):
     """make a python command line"""
-    prompt = '(hbnb) '
+    prompt = "(hbnb)"
+
+    def emptyline(self):
+        """does nothing on empty line"""
+        pass
 
     def do_exit(self, arg):
         'Quit command to exit the program'
@@ -15,6 +18,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         'EOF command to exit program'
+        print("")
         return True
 
 
