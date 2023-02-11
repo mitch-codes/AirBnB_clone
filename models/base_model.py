59d0f45122af813d786eaca32ec62eb8454da942
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-02-11 09:32:36 +0300

    add **kwargs argument

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,17 @@ class BaseModel:
     
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """constructor class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if (key == created_at or key == updated_at):
+                    self.key = value
+                else:
+                    self.key = value
 
     def __str__(self):
         """string representation of an object"""
