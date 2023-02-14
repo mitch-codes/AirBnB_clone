f46422149165c309ce6b7f7778e2e51ae97c00c6
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-02-14 11:28:39 +0300

    create serialization class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,52 +6,29 @@ deserializes JSON types
 
 import json
 from models.base_model import BaseModel
-from models.user import User
-
 
 class FileStorage:
-    """
-    Custom class for file storage
-    """
-
-    __file_path = "file.json"
+    """class used to serialize and deserialize objects into files"""
+    
+    __file_path = ""
     __objects = {}
-
+    
     def all(self):
-        """
-        Returns dictionary representation of all objects
-        """
-        return self.__objects
-
-    def new(self, object):
-        """sets in __objects the object with the key
-        <object class name>.id
-
-        Args:
-            object(obj): object to write
-
-        """
-        self.__objects[object.__class__.__name__ + '.' + str(object)] = object
-
+        """return dictionary of objects"""
+        return (__objects)
+    
+    def new(self, obj):
+        """sets __object width object and object.id as key"""
+        __objects[str(odj.id)] = obj
+    
     def save(self):
-        """
-        serializes __objects to the JSON file
-        (path: __file_path)
-        """
-        with open(self.__file_path, 'w+') as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()
-                       }, f)
+        """write json data into file"""
+        with open(__file_path, w) as f:
+            data = json.dumps(__objects)
+            f.write(data)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects, if the JSON
-        file exists, otherwise nothing happens)
-        """
-        try:
-            with open(self.__file_path, 'r') as f:
-                dict = json.loads(f.read())
-                for value in dict.values():
-                    cls = value["__class__"]
-                    self.new(eval(cls)(**value))
-        except Exception:
-            pass
+        """deserialize json data in file"""
+        with open (__file_path, r) as f:
+            data = f.read()
+            __objects = json.loads(data)
