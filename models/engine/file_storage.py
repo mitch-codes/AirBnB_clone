40bd2732379b4d9d1e750d84cfe6b8bc7ff9d799
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-02-15 10:52:06 +0300

    change file paths

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,12 +24,12 @@ class FileStorage:
     
     def save(self):
         """write json data into file"""
-        with open(__file_path, w) as f:
+        with open(self.__file_path, w) as f:
             data = json.dumps(self.__objects)
             f.write(data)
 
     def reload(self):
         """deserialize json data in file"""
-        with open (__file_path, r) as f:
+        with open (self.__file_path, r) as f:
             data = f.read()
             self.__objects = json.loads(data)
