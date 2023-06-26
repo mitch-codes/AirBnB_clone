df4ff12250cb99b72bed6917394f780932f9af74
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-06-26 10:53:03 +0300

    fix error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,17 +21,16 @@ class FileStorage:
     def new(self, obj):
         """sets __object width object and object.id as key"""
         myId = obj.__class__.__name__ + "." + obj.id
-        #self.__objects[myId] = obj.to_dict()
+        #self.__objects[myId] = obj
         self.__objects[myId] = obj
     
     def save(self):
         """write json data into file"""
+        mydicts = {}
+        for key in self.__objects:
+            mydicts[key] = self.__objects[key].to_dict()
+        data = json.dumps(mydicts)
         with open(self.__file_path, "w+") as f:
-            mydicts = {}
-            for key in self.__objects:
-                mydicts[key] = self.__objects[key].to_dict()
-            #data = json.dumps(self.__objects)
-            data = json.dumps(mydicts)
             f.write(data)
 
     def reload(self):
