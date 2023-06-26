da5b83e16e02cbaaf21327452e068dc5e27eefc8
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-06-26 04:30:00 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,45 +21,19 @@ class FileStorage:
     def new(self, obj):
         """sets __object width object and object.id as key"""
         myId = obj.__class__.__name__ + "." + obj.id
-        #self.__objects[myId] = obj.to_dict()
-	self.__objects[myId] = obj
+        self.__objects[myId] = obj.to_dict()
     
     def save(self):
         """write json data into file"""
-        """with open(self.__file_path, "w+") as f:
+        with open(self.__file_path, "w+") as f:
             data = json.dumps(self.__objects)
             f.write(data)
-	"""
-	my_obj_dict = {}
-        for key in self.__objects:
-            my_obj_dict[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(my_obj_dict, f)
 
     def reload(self):
         """deserialize json data in file"""
-        """try:
+        try:
             with open(self.__file_path, "r") as f:
                 dict = json.loads(f.read())
                 self.__objects.update(dict)
         except:
             pass
-	"""
-	my_dict = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-            }
-        if not os.path.isfile(self.__file_path):
-            return
-        with open(self.__file_path, "r") as file_path:
-            objects = json.load(file_path)
-            self.__objects = {}
-            for key in objects:
-                name = key.split(".")[0]
-                self.__objects[key] = my_dict[name](**objects[key])
-
