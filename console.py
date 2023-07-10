562a76ce0b76082c59c182ec1836c7661079bca7
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-07-10 10:40:29 +0300

    use console to create new object

--- console.py
+++ console.py
@@ -5,11 +5,14 @@ Contains the entry point of the command interpreter
 """
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """a class that inherits from cmd"""
 
     prompt = "(hbnb) "
+    classNames = ["BaseModel"]
 
     def do_EOF(self, line):
         """exit the program"""
@@ -18,7 +21,18 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
-    
+
+    def do_create(self, className):
+        """create a new object and save it"""
+        if className and className in self.classNames:
+            my_model = BaseModel()
+            my_model.save()
+            print(my_model.id)
+        elif className:
+            print("class doesn't exist")
+        else:
+            print("class name missing")
+
     def emptyline(self):
         """action to take on empty line"""
         pass
