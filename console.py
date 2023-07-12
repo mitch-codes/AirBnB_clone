3e7b1a9d9b276e6f8649ffd700e4c38604256813
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-07-12 11:48:26 +0300

    import User class

--- console.py
+++ console.py
@@ -7,6 +7,7 @@ Contains the entry point of the command interpreter
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User 
 
 class HBNBCommand(cmd.Cmd):
     """a class that inherits from cmd"""
