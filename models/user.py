4a3ea8ff4f382ed0ca820ab3cab215be560d46b9
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-07-12 09:35:56 +0300

    fix the indentation

--- models/user.py
+++ models/user.py
@@ -1,1 +1,15 @@
+#!/user/bin/python3
+"""
+add user information to json data
+"""
 
+from models.base_model import BaseModel
+import json
+
+class User(BaseModel):
+    """this is a subclass of BaseModel"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
