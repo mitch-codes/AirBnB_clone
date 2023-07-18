a4ed0507969801ed98a6754c0d426d9e04cf79e4
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-07-18 09:05:05 +0300

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+"""
+add Amenity information to json data
+"""
+
+from models.base_model import BaseModel
+import json
+
+class Amenity(BaseModel):
+    """this is a subclass of BaseModel"""
+
+    name = ""
+  
