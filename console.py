6586d1c560c69ccc85d84a8d6ba313792009dd6a
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-07-18 09:41:09 +0300

    import the new classes

--- console.py
+++ console.py
@@ -7,7 +7,12 @@ Contains the entry point of the command interpreter
 import cmd
 from models import storage
 from models.base_model import BaseModel
-from models.user import User 
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """a class that inherits from cmd"""
